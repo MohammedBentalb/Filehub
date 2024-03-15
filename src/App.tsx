40272cd9bbbf10305d00c5d073bf82c1e83ccc02
mohammed bentalb <mohammedbentalb2000@gmail.com>
2024-03-15 17:38:51 +0000

    warning/info start page

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,21 @@ import SignIn from './pages/auth/signIn/SignIn';
 import SignUp from './pages/auth/signUp/SignUp';
 import Main from './pages/main/Main';
 import NotFound from './pages/not-found/NotFound';
+import NavBarLayout from './layouts/NavBarLayout';
 
 function App() {
   const route = createBrowserRouter([
     { path: '/', element: <Home /> },
-    { path: '/main', element: <Main /> },
+    {
+      path: '/',
+      element: <NavBarLayout />,
+      children: [
+        { path: '/main', element: <Main /> },
+        { path: '/*', element: <NotFound /> },
+      ],
+    },
     { path: '/auth/sign-in', element: <SignIn /> },
     { path: '/auth/sign-up', element: <SignUp /> },
-    { path: '/*', element: <NotFound /> },
   ]);
 
   return (
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,51 @@
+import { useEffect } from 'react';
+import style from './home.module.scss';
+import { Link } from 'react-router-dom';
+
 function Home() {
-  return <div>this is the home page</div>;
+  useEffect(() => {
+    const wrapper = document.getElementById('wrapper');
+    if (!wrapper) return;
+    const elements = Array.from(wrapper.children);
+    elements.forEach((item) => {
+      wrapper.append(item.cloneNode(true));
+    });
+    console.log(wrapper);
+  }, []);
+
+  return (
+    <main className={style.main}>
+      <header className={style.header}>
+        <h1 className={style.firstH1}>
+          File<span>h</span>ub
+        </h1>
+        <p className={style.first_paragraph}>
+          <span>
+            File<span>h</span>ub
+          </span>{' '}
+          is a Portfolio project that has been made only to show case experience
+          in web development using a set of dev Tools
+        </p>
+        <p className={style.second_paragraph}>
+          we don't recommend <span>downloading</span> files recklessly with out
+          knowing the Origin
+        </p>
+      </header>
+      <div className={style.slider} id="slider">
+        <ul className={style.wrapper} id="wrapper">
+          <li>Zod</li>
+          <li>NodeJs/Express</li>
+          <li>Mongoose</li>
+          <li>React-Query</li>
+          <li>React Hook Form</li>
+          <li>React Router DOM</li>
+        </ul>
+      </div>
+      <Link to={'/main'} className={style.getStarted}>
+        Get Started
+      </Link>
+    </main>
+  );
 }
 
 export default Home;
